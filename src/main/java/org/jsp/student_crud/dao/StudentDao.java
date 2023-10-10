fa2515532d3fbaa624a9057b4c6cb25d9f43c947
Harshacharr <141137476+Harshacharr@users.noreply.github.com>
2023-10-10 21:06:39 +0530

    firstcommit

---
+++ src/main/java/org/jsp/student_crud/dao/StudentDao.java
@@ -0,0 +1,33 @@
+package org.jsp.student_crud.dao;
+
+import java.util.List;
+
+import org.jsp.student_crud.dto.Student;
+import org.jsp.student_crud.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class StudentDao {
+
+    @Autowired
+    StudentRepository studentRepository;
+
+    public Student save(Student student) {
+        return studentRepository.save(student);
+    }
+
+    public List<Student> saveStudents(List<Student> students) {
+        return studentRepository.saveAll(students);
+    }
+ 
+
+    public List<Student> findAll() {
+        return studentRepository.findAll();
+    }
+
+    public Student findById(int id) {
+        return studentRepository.findById(id).orElse(null);
+    }
+
+}
